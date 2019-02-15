5d0a70deca83a45c3b05ad18e0ec2fcc0b40b17a
Enoch <enoinoo@gmail.com>
2019-02-15 14:43:26 -0500

    SignIn Screen and SignOut Button set up

---
+++ components/SignInScreen/SignIn.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+
+class SignIn extends Component {
+  render() {
+    return (
+      <>
+        <View style={styles.container}>
+          <TextInput style={styles.textInput} placeholder="username or email" />
+          <TextInput
+            style={styles.textInput}
+            secureTextEntry={true}
+            placeholder="password"
+          />
+        </View>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {},
+  textInput: {
+    marginBottom: 20,
+    height: 40,
+    fontSize: 20,
+    borderColor: "gray",
+    borderBottomWidth: 1,
+    marginTop: 5
+  }
+});
+
+export default SignIn;
--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
-import { AsyncStorage, Button, StyleSheet, View } from "react-native";
+import {
+  AsyncStorage,
+  Button,
+  StyleSheet,
+  ScrollView,
+  View
+} from "react-native";
+import Signin from "../components/SignInScreen/SignIn";
 
 class SignInScreen extends Component {
   static navigationOptions = {
     title: "Please sign in"
   };
-
   _signInAsync = async () => {
     await AsyncStorage.setItem("userToken", "abc");
     this.props.navigation.navigate("Main");
@@ -13,16 +19,23 @@ class SignInScreen extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Button title="Sign in!" onPress={this._signInAsync} />
-      </View>
+      <ScrollView style={styles.scollContainer}>
+        <Signin />
+        <View style={styles.buttonContainer}>
+          <Button title="Sign in!" onPress={this._signInAsync} />
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  scollContainer: {
     flex: 1,
+    padding: 30,
+    flexDirection: "column"
+  },
+  buttonContainer: {
     alignItems: "center",
     justifyContent: "center"
   }
