b79cea35ae60521557f6bdbf0635aa23dd28b63a
Kody Moseley <kody.moseley@maine.edu>
2019-02-27 16:15:05 -0500

    Might have changed nav-bar to the top

--- Front-End/navigation/MainTabNavigator.js
+++ Front-End/navigation/MainTabNavigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import { createStackNavigator, createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
@@ -53,7 +53,7 @@ SettingsStack.navigationOptions = {
   ),
 };
 
-export default createBottomTabNavigator({
+export default createMaterialTopTabNavigator({
   HomeStack,
   LinksStack,
   SettingsStack,
