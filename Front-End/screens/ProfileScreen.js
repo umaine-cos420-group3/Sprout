f4fd723eb76eb05c3dc901e91bcb0d5525d88b0a
Noah Monto <noah.monto@maine.edu>
2019-03-14 14:38:48 -0400

    started the bio section

--- Front-End/screens/ProfileScreen.js
+++ Front-End/screens/ProfileScreen.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { StyleSheet, ScrollView } from "react-native";
-import { ExpoConfigView } from "@expo/samples";
+import { StyleSheet, ScrollView, Text} from "react-native";
+//import { ExpoConfigView } from "@expo/samples";
 import SignOutButton from "../components/ProfileScreen/SignOutButton";
+import { Avatar, Card, Button, Icon } from 'react-native-paper';
 
 const styles = StyleSheet.create({
   scollContainer: {
     flex: 1,
-    flexDirection: "column"
+    flexDirection: "column",
+    backgroundColor: 'lightgrey',
+    padding: 50,
+    
+
   }
 });
 
 export default class ProfileScreen extends React.Component {
   static navigationOptions = {
-    title: "app.json"
+    title: "Profile",
+    headerStyle: {
+      backgroundColor: 'lightgrey',
+      borderBottomColor: 'lightgrey',
+    },
+    headerTitleStyle: {
+      fontSize: 40,
+      textAlign:"center", 
+      flex:1 
+    },
   };
   _signOut = () => {
     this.props.navigation.navigate("Auth");
@@ -23,7 +37,17 @@ export default class ProfileScreen extends React.Component {
      * content, we just wanted to give you a quick view of your config */
     return (
       <ScrollView style={styles.scollContainer}>
-        <ExpoConfigView />
+        <Card>
+        <Card.Title title="Susan" subtitle="Bio" left={(props) => <Avatar.Icon {...props} icon="person" />} />
+          <Card.Content>
+            <Text style={styles.paragraph}>
+              Tell people something about yourself
+            </Text>
+            <Button icon="edit" mode="contained" onPress={this._editBioAsync}>
+            Edit
+            </Button>
+         </Card.Content>
+        </Card>
         <SignOutButton signOutFunction={this._signOut} />
       </ScrollView>
     );
