16c939942df1faaeec6647ba36c4decf1b0039cc
Matthew Loewen <matthewlanimates@gmail.com>
2019-04-14 17:43:20 -0400

    backend setup using mysql library

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,12 +1,26 @@
-import express from 'express'; 
+// import express from 'express'; 
 
-const Sprout = express(); 
+// const Sprout = express(); 
 
-Sprout.get('/', (req, res) =>
-				res.send('Sprout main page'))
+// Sprout.get('/', (req, res) =>
+// 				res.send('Sprout main page'))
 
-const server = Sprout.listen(3000, ()  => {
-	const { address, port } = server.address(); 
-	console.log(`add:${address}por:${port}`);
+// const server = Sprout.listen(3000, ()  => {
+// 	const { address, port } = server.address(); 
+// 	console.log(`add:${address}por:${port}`);
 
+// });
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "matt",
+	password: "password",
+	database: "sprout"
+  });
+
+con.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected!");
 });
