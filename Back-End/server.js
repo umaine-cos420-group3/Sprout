8c16ba0dc62397450bbe3a33cf24d8153064b666
Matthew Loewen <matthewlanimates@gmail.com>
2019-04-15 00:09:13 -0400

    working server

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,26 +1,26 @@
-// import express from 'express'; 
-
-// const Sprout = express(); 
-
-// Sprout.get('/', (req, res) =>
-// 				res.send('Sprout main page'))
-
-// const server = Sprout.listen(3000, ()  => {
-// 	const { address, port } = server.address(); 
-// 	console.log(`add:${address}por:${port}`);
-
-// });
-
+var express = require('express'); 
 var mysql = require('mysql');
+const app = express(); 
 
-var con = mysql.createConnection({
+
+var connection = mysql.createConnection({
 	host: "localhost",
 	user: "matt",
 	password: "password",
 	database: "sprout"
   });
 
-con.connect(function(err) {
+
+app.get('/', (req, res) =>
+	res.send('Sprout main page'))
+
+
+const server = app.listen(3000, ()  => {
+	const { address, port } = server.address(); 
+	console.log(`add:${address}por:${port}`);
+});
+
+connection.connect(function(err) {
 	if (err) throw err;
 	console.log("Connected!");
 });
